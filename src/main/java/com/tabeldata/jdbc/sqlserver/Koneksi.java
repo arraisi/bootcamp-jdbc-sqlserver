b7bdba88eeb18becdbd3cf3e15c8cc92c8e15614
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-04-26 14:26:41 +0700

    menampilkan data dari tabel ke java bean

--- src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
+++ src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
@@ -1,13 +1,12 @@
 package com.tabeldata.jdbc.sqlserver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Koneksi {
 
+    // if database server on windows jdbc:sqlserver://LOKASI_DATABASE:PORT_DATABASE;databaseName=NAMA_DATABASE;instanceName=SQLSERVER2017;
     //    format jdbc url yaitu jdbc:sqlserver://LOKASI_DATABASE:PORT_DATABASE;databaseName=NAMA_DATABASE
-    private static final String URL = "jdbc:sqlserver://192.168.100.50:1433;databaseName=JDBC_DIMAS;";
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=JDBC_DIMAS;";
     private static final String username = "sa";
     private static final String password = "PasswordnyaSA2018";
 
@@ -23,7 +22,20 @@ public class Koneksi {
 //            System.out.println("Tidak dapat koneksi ke database kerena username atau password salah");
 //        }
 
-        Connection connection = DriverManager.getConnection(URL, username, password);
+        Connection koneksi = DriverManager.getConnection(URL, username, password);
+        Statement perintah = koneksi.createStatement();
+        ResultSet hasil = perintah.executeQuery("select ID, JUDUL_BUKU, NAMA_PENGARANG, ISBN from Buku");
+//        ambil data per baris
+        while (hasil.next()) {
+            String namaPengarang = hasil.getString("NAMA_PENGARANG");
+            String judulBuku = hasil.getString("JUDUL_BUKU");
+            String isbn = hasil.getString(4);
+            Integer id = hasil.getInt(1);
 
+            System.out.println("namaPengarang : " + namaPengarang + ", judulBuku: " + judulBuku + ", isbn: " + isbn);
+        }
+        hasil.close();
+        perintah.close();
+        koneksi.close();
     }
 }
