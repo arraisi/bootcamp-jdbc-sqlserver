d9e720edb8d2da33a7eb1a00185b3701ee9c743d
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-04-26 11:46:25 +0700

    membuat koneksi ke database

--- src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
+++ src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
@@ -7,20 +7,23 @@ import java.sql.SQLException;
 public class Koneksi {
 
     //    format jdbc url yaitu jdbc:sqlserver://LOKASI_DATABASE:PORT_DATABASE;databaseName=NAMA_DATABASE
-    private static final String URL = "jdbc:sqlserver://192.168.100.50:1433;databaseName=DBTEST;";
+    private static final String URL = "jdbc:sqlserver://192.168.100.50:1433;databaseName=JDBC_DIMAS;";
     private static final String username = "sa";
     private static final String password = "PasswordnyaSA2018";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        try {
-            // untuk membuat koneksi ke database
-            Connection connection = DriverManager.getConnection(URL, username, password);
-            System.out.println("Database terkoneksi");
-        } catch (SQLException sqle) {
-            // handling ketika terjadi error koneksi ke database
-            sqle.printStackTrace();
-        }
+//        try {
+//            // untuk membuat koneksi ke database
+//            Connection connection = DriverManager.getConnection(URL, username, password);
+//            System.out.println("Database terkoneksi");
+//        } catch (SQLException sqle) {
+//            // handling ketika terjadi error koneksi ke database
+////            sqle.printStackTrace();
+//            System.out.println("Tidak dapat koneksi ke database kerena username atau password salah");
+//        }
+
+        Connection connection = DriverManager.getConnection(URL, username, password);
 
     }
 }
