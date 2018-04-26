dd4befaa4290be0edf41027c62b9ff51d197c3e1
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-04-26 15:46:26 +0700

    merubah data yang ada di database

--- src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
+++ src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
@@ -13,17 +13,12 @@ public class Koneksi {
     public static void main(String[] args) throws SQLException {
 
         Connection koneksi = DriverManager.getConnection(URL, username, password);
-        PreparedStatement perintah = koneksi.prepareStatement("insert into Buku ( JUDUL_BUKU, NAMA_PENGARANG, PENERBIT_ID, TAHUN_TERBIT, TANGGAL_PUBLISH, ISBN, TERSEDIA, CREATED_DATE, LAST_UPDATE_DATE, CREATED_BY, UPDATED_BY)\n" +
-                "values (?, ?, ?, ?, ?, ?, ?, ?, null, ?, null);");
-        perintah.setString(1, "PHP Programing");
-        perintah.setString(2, "Putri");
-        perintah.setInt(3, 1);
-        perintah.setInt(4, 2017);
-        perintah.setDate(5, Date.valueOf("2017-08-20"));
-        perintah.setString(6, "1234-12345-2343435");
-        perintah.setBoolean(7, true);
-        perintah.setTimestamp(8, Timestamp.valueOf("2017-03-10 10:00:00"));
-        perintah.setString(9, "OP");
+        PreparedStatement perintah = koneksi.prepareStatement("update Buku\n" +
+                "set NAMA_PENGARANG = ?, LAST_UPDATE_DATE = sysdatetime(), UPDATED_BY = ?\n" +
+                "where ID = ?");
+        perintah.setString(1, "Dimas Maryanto");
+        perintah.setString(2, "OP");
+        perintah.setInt(3, 2);
         perintah.executeUpdate();
         perintah.close();
         koneksi.close();
