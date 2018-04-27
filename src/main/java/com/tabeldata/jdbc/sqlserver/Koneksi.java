a5a46185bf8d962704c832dbf0932c9d730cf576
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-04-27 15:00:00 +0700

    init project exam

--- src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
+++ src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
@@ -1,16 +1,10 @@
 package com.tabeldata.jdbc.sqlserver;
 
-import com.tabeldata.jdbc.sqlserver.model.Buku;
-import com.tabeldata.jdbc.sqlserver.service.BukuService;
-
-import java.sql.*;
-import java.util.Arrays;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Koneksi {
-
-    // if database server on windows jdbc:sqlserver://LOKASI_DATABASE:PORT_DATABASE;databaseName=NAMA_DATABASE;instanceName=SQLSERVER2017;
-    //    format jdbc url yaitu jdbc:sqlserver://LOKASI_DATABASE:PORT_DATABASE;databaseName=NAMA_DATABASE
 
     public static Connection getKoneksiKeDB() throws SQLException {
         String username = "sa";
@@ -19,13 +13,7 @@ public class Koneksi {
         return DriverManager.getConnection(URL, username, password);
     }
 
-
-
     public static void main(String[] args) throws SQLException {
-
-//        List<String> daftarKatogori = Arrays.asList("Pemograman", "Ilmu Komputer Dasar", "Networking", "Sastra", "Fisika", "Elektro");
-        BukuService serviceBuku = new BukuService();
-        serviceBuku.saveData(new Buku("Spiderman", "asdfkj", 1, 2017, Date.valueOf("2018-10-03"), "23423423-234234-234324", false));
 
     }
 }
