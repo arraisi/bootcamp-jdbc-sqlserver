d848e574d88d9a547701a029e27c4bc2d0b45097
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-04-27 10:33:13 +0700

    rollback handle by self

--- src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
+++ src/main/java/com/tabeldata/jdbc/sqlserver/Koneksi.java
@@ -10,31 +10,44 @@ public class Koneksi {
     private static final String username = "sa";
     private static final String password = "PasswordnyaSA2018";
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
-        Connection koneksi = DriverManager.getConnection(URL, username, password);
+        Connection koneksi = null;
+        try {
+            koneksi = DriverManager.getConnection(URL, username, password);
 //        transactional auto commit dimatikan
-        koneksi.setAutoCommit(false);
-        PreparedStatement perintah = koneksi.prepareStatement("update Buku\n" +
-                "set NAMA_PENGARANG = ?, LAST_UPDATE_DATE = sysdatetime(), UPDATED_BY = ?\n" +
-                "where ID = ?");
-        perintah.setString(1, "Dimas");
-        perintah.setString(2, "OP1");
-        perintah.setInt(3, 2);
-        perintah.executeUpdate();
+            koneksi.setAutoCommit(false);
+            PreparedStatement perintah = koneksi.prepareStatement("update Buku\n" +
+                    "set NAMA_PENGARANG = ?, LAST_UPDATE_DATE = sysdatetime(), UPDATED_BY = ?\n" +
+                    "where ID = ?");
+            perintah.setString(1, "Dimas");
+            perintah.setString(2, "OP1");
+            perintah.setInt(3, 2);
+            perintah.executeUpdate();
 
-        perintah = koneksi.prepareStatement("update Buku\n" +
-                "set TAHUN_TERBIT = ?, LAST_UPDATE_DATE = sysdatetime(), UPDATED_BY = ?\n" +
-                "where ID = ?");
-        perintah.setInt(1, 2011);
-        perintah.setString(2, "OP");
-        perintah.setInt(3, 2);
-        perintah.executeUpdate();
+            perintah = koneksi.prepareStatement("update Buku\n" +
+                    "set TAHUN_TERBIT = ?, LAST_UPDATE_DATE = sysdatetime(), UPDATED_BY = ?\n" +
+                    "where ID = ?");
+            perintah.setInt(1, 2011);
+            perintah.setString(2, "OP");
+            perintah.setInt(3, 2);
+            perintah.executeUpdate();
 
 //      simpan data secara permanen
-        koneksi.commit();
-
-        perintah.close();
-        koneksi.close();
+            koneksi.commit();
+            System.out.println("Berhasil di update!");
+            perintah.close();
+            koneksi.close();
+        } catch (SQLException sqle) {
+            if (koneksi != null) {
+                try {
+                    koneksi.rollback();
+                    System.out.println("Rollback berhasil!");
+                } catch (SQLException sqle2) {
+                    System.out.println("Tidak dapat melakukan rollback");
+                    sqle2.printStackTrace();
+                }
+            }
+        }
     }
 }
